9c4e9862b240fcf26c17ab1b91d4e36b665520ac
sarocha2449 <67030354@kmitl.ac.th>
2024-11-21 12:25:37 +0000

    downcase_it.py

--- cell05/ex07/downcase_it.py
+++ cell05/ex07/downcase_it.py
@@ -2,9 +2,12 @@ import sys
 
 def main():
     if len(sys.argv) > 1:
-        input_text = " ".join(sys.argv[1:])
+        text = " ".join(sys.argv[1:])
+        print(text.lower())
+    elif len(sys.argv) <= 1:
+        print("none")
     else:
-        input_text = sys.stdin.read()
-    print(input_text.lower())
+        text = sys.stdin.read()
+        print(text.lower())
 if __name__ == "__main__":
     main()
