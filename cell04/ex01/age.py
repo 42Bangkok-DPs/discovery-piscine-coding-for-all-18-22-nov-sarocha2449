6ae752bcd6108bb6472cefde662a47c72162bd76
sarocha2449 <67030354@kmitl.ac.th>
2024-11-21 04:10:19 +0000

    age.py

---
+++ cell04/ex01/age.py
@@ -0,0 +1,8 @@
+age = int(input("please tell me your age: "))
+print("You are currently",age,"years old.")
+ageone = age + 10
+agetwo = age + 20
+agethree = age + 30
+print(f"In 10 years, you'll be {ageone} years old.")
+print(f"In 20 years, you'll be {agetwo} years old.")
+print(f"In 30 years, you'll be {agethree} years old.")
