fdead5e22a9151b15f1b38705057005e9d311288
sarocha2449 <67030354@kmitl.ac.th>
2024-11-21 12:22:50 +0000

    upcase_it.py

--- cell05/ex06/upcase_it.py
+++ cell05/ex06/upcase_it.py
@@ -1,10 +1,12 @@
 import sys
-
 def main():
     if len(sys.argv) > 1:
-        input_text = " ".join(sys.argv[1:])
+        word = " ".join(sys.argv[1:])
+        print(word.upper())
+    elif len(sys.argv) <= 1:
+        print("none")
     else:
-        input_text = sys.stdin.read()
-    print(input_text.upper())
+        word = sys.stdin.read()
+        print(word.upper())
 if __name__ == "__main__":
     main()
